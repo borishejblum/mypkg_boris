24fe96740fbbab773936bb1b26225cf419114007
Cours <cours@MacBook-Pro-de-Boris.local>
2019-09-30 14:19:31 +0200

    initial commit

---
+++ R/mylog.R
@@ -0,0 +1,25 @@
+#' My logarithm function
+#'
+#'This is a function that
+#'computes the logarithm of a vector
+#'
+#' @param x a vector of numbers
+#'
+#' @return a vector of the same length
+#' as \code{x} with the \code{log} of
+#' \code{x}.
+#'
+#' @export
+#'
+#' @examples
+#' log(1)
+#' log(2)
+#'
+mylog <- function(x){
+  n <- length(x)
+  res <- numeric(length = n)
+  for(i in 1:n){
+    res[i] <- log(x[i])
+  }
+  return(res)
+}
---
+++ tests/testthat/test-mylog.R
@@ -0,0 +1,4 @@
+test_that("mylog works", {
+  expect_equal(mylog(1), 0)
+  expect_equal(mylog(exp(2)), 1)
+})
